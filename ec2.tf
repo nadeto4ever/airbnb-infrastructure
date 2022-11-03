569d28d03b4e6d0a2baa52e571c0cdb962172d8a
Adetoun A <nadeto4ever@yahoo.com>
2022-11-03 19:41:51 -0400

    fix checkov 126, 135

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,8 @@
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  monitoring = true
+  ebs_optimized = true
 
   tags = {
     Name = "airbnb-web-server"
