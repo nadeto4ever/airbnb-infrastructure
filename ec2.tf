f05069d29f695bb212d382811b7f983c039f7ade
Adetoun A <nadeto4ever@yahoo.com>
2022-11-03 20:13:57 -0400

    checkov scan fix

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,9 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
   monitoring = true
   ebs_optimized = true
+  root_block_device {
+    encypted = true
+  }
 
   tags = {
     Name = "airbnb-web-server"
